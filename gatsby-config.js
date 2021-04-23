58650c1632c9a2496f6907aefa4c8a2ecb881e93
keeks5456 <Akikogreen08@gmail.com>
2021-04-22 21:22:13 -0700

    images are rendering perfectly now

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,7 +46,7 @@ module.exports = {
           {
             resolve: 'gatsby-remark-images',
             options:{
-              maxWidth: 750,
+              maxWidth: 800,
               linkImagesToOriginal: false
             }
           }
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -4,14 +4,11 @@ import { graphql } from 'gatsby'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import Head from '../components/head'
 
-
-const { BLOCKS } = require('@contentful/rich-text-types')
-
 export const query = graphql`
 query ($slug: String!) {
   contentfulBlogPost(slug: { eq: $slug } ) {
     title
-    image {
+    img {
       file {
         url
       }
@@ -27,28 +24,16 @@ query ($slug: String!) {
 
 const Blog = (props) => {
 
-  // const options = {
-  //   renderNode: {
-  //     [BLOCKS.EMBEDDED_ASSET]: node => {
-  //       const imageId = node.data.target.sys.id
-  //       console.log(node.data)
-  //       const {
-  //         file: {url}, 
-  //         title
-  //      } = props.data.contentfulBlogPost.image.find(({contentful_id: id}) => id === imageId);
-  //       return <img alt={title} src={url} />
-  //     }
-  //   }
-  // }
-  
+
 console.log(props.data)
+// debugger
   return (
     <Layout>
+      <img src={props.data.contentfulBlogPost.img.file.url} alt="yur mom"></img>
       <Head title={props.data.contentfulBlogPost.title}/>
-      <img src={props.data.contentfulBlogPost.image[0].file.url} alt="yur mom"></img>
-      <h1>{props.data.contentfulBlogPost.title}</h1>
-      <p>{props.data.contentfulBlogPost.publishedDate}</p>
+      {/* <h1>{props.data.contentfulBlogPost.title}</h1> */}
       {documentToReactComponents(JSON.parse(props.data.contentfulBlogPost.body.raw))}
+      <p>{props.data.contentfulBlogPost.publishedDate}</p>
     </Layout>
   );
 };
