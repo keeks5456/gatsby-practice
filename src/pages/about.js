c97055bdf125385d33413458c01492bb5411fc9e
Akiko Green <akikogreen08@gmail.com>
2021-05-14 22:15:41 -0700

    working on about section

--- src/pages/about.js
+++ src/pages/about.js
@@ -23,7 +23,7 @@ const About = () => {
       <h2>About The Creator</h2>
       <p>My love for all things anime started when I was a young girl. My first anime I fell in love with was <b>Inuyasha</b>, a show about a half-demon named <i>Inuyasha</i> who embarks on an adventure with <i>Kagome</i>, <i>Sango</i>, <i>Shippo</i>, and <i>Miroku</i> in hopes of finding the Shikan Jewel to turn him into a full fledged demon. As I grew older, my love for anime grew as well. From the older 1990's anime like Inuyasha and Yu-Yu-Hakusho to the now more modern, popular anime's like Demon Slayer, Attack on Titans, and Boruto, anime has gradually made long lasting positive impacts on my life.</p>
       <br/>
-      <p>I started my software engineer journey in January 2020. I began learning HTML & CSS. Eventually, in the same year I graduated from Flatiron School. Now that I have graduated, I am now on the hunt for my first ever Software Engineer Role. This career change has been a roller coaster of a ride.</p>
+      <p>One way anime has made an impact on my life is the main character underdog archetype. I have always resonated with this from my own personal battles of being seen as the underdog. During my journey to becoming a Software Engineer...</p>
       <Link to="/contact">Contact me here</Link>
       </div>
     </Layout>
